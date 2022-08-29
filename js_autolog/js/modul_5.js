93391702dcc8549b2a1ef8e3de822ddb6421f202
Andrew <andrew.levik@gmail.com>
2022-08-29 14:34:00 +0200

    update modul_5

--- js_autolog/js/modul_5.js
+++ js_autolog/js/modul_5.js
@@ -95,16 +95,111 @@
 // Task_4
 // Измени код так, чтобы объект parent стал прототипом для объекта в переменной сhild.
 
-const parent = {
-  name: "Stacey",
-  surname: "Moore",
-  age: 54,
-  heritage: "Irish",
-};
-// Change code below this line
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+// // Change code below this line
 
-const child = {};
+// const child = = Object.create(parent);
 
-// Change code above this line
-child.name = "Jason";
-child.age = 27;
+// // Change code above this line
+// child.name = "Jason";
+// child.age = 27;
+
+
+// Task_10
+// Напиши класс Storage, который будет создавать объекты для управления складом товаров.
+// Класс ожидает только один аргумент - начальный массив товаров, который записывается на создаваемый
+// объект в свойство items.
+
+// Объяви следующие методы класса:
+
+//   getItems() - возвращает массив текущих товаров в свойстве items объекта который вызывает этот метод.
+//   addItem(newItem) - принимает новый товар newItem и добавляет его в массив товаров в свойстве items объекта
+// который вызывает этот метод.
+//   removeItem(itemToRemove) - принимает товар itemToRemove и удаляет его из массива товаров в свойстве
+// items объекта который вызывает этот метод.
+// Под комментарием мы добавили инициализацию экземпляра и вызовы методов в той последовательности,
+//   в которой твой код будут проверять тесты.Пожалуйста ничего там не меняй.
+
+// class Storage {
+//   constructor (items){
+//     this.items = items;
+//   }
+
+// getItems() {
+//   return this.items;
+// }
+
+// addItem(newItem) {
+//   return this.items.push(newItem);
+//   }
+  
+//   removeItem(itemToRemove) {
+//     this.items.forEach((item, index) => {
+//       if (item === itemToRemove) {
+//       return this.items.splice(index, 1)
+//     }
+//     });
+//    }
+// }
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// Task_11
+// Напиши класс StringBuilder, который принимает один параметр initialValue - произвольную строку, которая
+// записывается на создаваемый объект в свойство value.
+
+// Объяви следующие методы класса:
+
+// getValue() - возвращает текущее значение свойства value.
+// padEnd(str) - получает парметр str (строку) и добавляет её в конец значения свойства value объекта который
+// вызывает этот метод.
+// padStart(str) - получает парметр str (строку) и добавляет её в начало значения свойства value объекта который
+// вызывает этот метод.
+// padBoth(str) - получает парметр str (строку) и добавляет её в начало и в конец значения свойства value объекта
+// который вызывает этот метод.
+// Под комментарием мы добавили инициализацию экземпляра и вызовы методов в той последовательности,
+// в которой твой код будут проверять тесты. Пожалуйста ничего там не меняй.
+
+// class StringBuilder{
+//   constructor(value) {
+//     this.value = value;
+//   }
+
+//   getValue() {
+//     return this.value;
+//   }
+
+//   padEnd(str) {
+//     this.value += str;
+//   }
+
+//   padStart(str) {
+//     this.value = str + this.value;
+//   }
+
+//   padBoth(str) {
+//     this.value = str + this.value + str;
+//   }
+// }
+
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
